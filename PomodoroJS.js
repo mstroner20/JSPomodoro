7ee02f7cc0dfb8ffe2c57d22192fa3636a1c2a0d
Michael Stroner <Michaelstroner100@gmail.com>
2020-07-02 10:43:18 -0500

    Moved Script/adding styles to timer
    Moved the script to allow access to buttons.

--- PomodoroJS.js
+++ PomodoroJS.js
@@ -13,6 +13,14 @@ let timeLeft = TIME_LIMIT;
 
 let timerInterval = null;
 
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  }
+};
+
+let remainingPathColor = COLOR_CODES.info.color;
+
 startButton.addEventListener('click', startTimer);
 
 document.getElementById("pomodoroTimer").innerHTML = `
@@ -20,6 +28,17 @@ document.getElementById("pomodoroTimer").innerHTML = `
   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
     <g class="base-timer__circle">
       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
     </g>
   </svg>
   <span id="base-timer-label" class="base-timer__label">
