2ac68a67eba5703c304f1a817d86d7ee98a20405
Michael Stroner <Michaelstroner100@gmail.com>
2020-07-02 20:15:03 -0500

    Fixed timer speed
    I think this should fix it even tho it looks like I only indented

--- PomodoroJS.js
+++ PomodoroJS.js
@@ -19,7 +19,7 @@ const COLOR_CODES = {
   }
 };
 
-let remainingPathColor = COLOR_CODES.info.color;
+let remainingPathColor = 	COLOR_CODES.info.color;
 
 startButton.addEventListener('click', startTimer);
 
@@ -29,16 +29,16 @@ document.getElementById("pomodoroTimer").innerHTML = `
     <g class="base-timer__circle">
       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
       <path
-        id="base-timer-path-remaining"
-        stroke-dasharray="283"
-        class="base-timer__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
+      id="base-timer-path-remaining"
+      stroke-dasharray="283"
+      class="base-timer__path-remaining ${remainingPathColor}"
+      d="
+        M 50, 50
+        m -45, 0
+        a 45,45 0 1,0 90,0
+        a 45,45 0 1,0 -90,0
+      "
+    ></path>
     </g>
   </svg>
   <span id="base-timer-label" class="base-timer__label">
